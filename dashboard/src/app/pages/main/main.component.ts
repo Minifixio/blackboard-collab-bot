471a84a39bf1cf2e9cf3ffcf6a509ba9e3a4357d
Minifixio <emile@le-gallic.com>
2020-04-30 19:19:39 +0200

    new : socket handle for the dashboard

--- dashboard/src/app/pages/main/main.component.ts
+++ dashboard/src/app/pages/main/main.component.ts
@@ -4,6 +4,8 @@ import { AuthService } from 'src/app/services/auth.service';
 import { HttpService } from 'src/app/services/http.service';
 import { SocketService } from 'src/app/services/socket.service';
 import { Observable } from 'rxjs';
+import { Bot } from 'src/app/models/Bot';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-main',
@@ -12,21 +14,34 @@ import { Observable } from 'rxjs';
 })
 export class MainComponent implements OnInit {
 
-  @ViewChild('commands', {static: true})
+  @ViewChild('commands')
   commands: CommandsListComponent;
 
   botStatusSocket: Observable<any>;
-  botUrl: string;
-  botName: string;
+  currentBot: Bot;
+
+  loading = false;
+  connected = false;
+
+  botUrlInput: string;
+  botNameInput: string;
 
   constructor(
     private auth: AuthService,
     private httpService: HttpService,
-    private socketService: SocketService
+    private socketService: SocketService,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
     this.socketService.setup();
+
+    this.httpService.get('bot').toPromise().then(bot => {
+      console.log(bot)
+      if (bot) {
+        this.currentBot = bot;
+      }
+    });
 
     if (!this.botStatusSocket) {
       this.botStatusSocket = this.socketService.subscribe('bot-status');
@@ -36,20 +51,45 @@ export class MainComponent implements OnInit {
     }
   }
 
-  startBot() {
-    this.httpService.post('start', {
-      name: this.botName,
-      url: this.botUrl,
-      commands: this.commands.selectedCommands()
+  async startBot() {
+
+    if (!this.botName || this.botUrl.includes('bbcollab.com')) {
+      this.showToastError('Veuillez remplir les champs correctement avant!');
+    } else {
+
+      this.currentBot = {
+        name: this.botName,
+        url: this.botUrl,
+        commands: this.commands.selectedCommands()
+      };
+
+      await this.httpService.post('start', this.currentBot).toPromise();
+
+      this.loading = true;
+    }
+  }
+
+  disconnectBot() {
+
+  }
+
+  showToastError(message: string) {
+    this.snackBar.open(message, null, {
+      duration: 5000
     });
   }
 
   socketCases(info) {
+    console.log(info);
     switch (info) {
       case 'connecting':
+        this.loading = false;
+        this.connected = false;
         break;
 
       case 'live':
+        this.loading = false;
+        this.connected = true;
         break;
 
       case 'wrong-url':
@@ -58,13 +98,18 @@ export class MainComponent implements OnInit {
       case 'error':
         break;
 
-      case 'bot-info':
+      case 'bot-infos':
+        this.currentBot = info;
         break;
 
       case 'setup-mic':
+        this.loading = false;
+        this.connected = false;
         break;
 
       case 'setup-mic-done':
+        this.loading = false;
+        this.connected = false;
         break;
     }
   }
